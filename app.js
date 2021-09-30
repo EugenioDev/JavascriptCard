2f0fce46cd0a694801e19ae1bf4d7233388bff35
Eugenio Marino <geggiodev@Air-di-Eugenio.homenet.telecomitalia.it>
2021-09-30 14:56:39 +0200

    Fixed Button

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const btn = document.querySelectorAll(".btn");
 // console.log(btn)
 for (i = 0; i < btn.length; i++) {
@@ -6,15 +7,18 @@ for (i = 0; i < btn.length; i++) {
     if (this.lastElementChild.innerHTML === "Follow") {
       this.lastElementChild.innerHTML = "Già Segui";
     } else if (this.lastElementChild.innerHTML === "Send Message") {
-      let textArea = document.querySelector(".textBox");
-      let textBox = document.createElement("textarea");
-      textBox.placeholder = "Your Message Here...";
-      textBox.classList.add("messageBox");
-      textBox.name = "message";
-      textBox.type = "text";
-      
-      textArea.appendChild(textBox);
-    }else {
+      if (document.querySelector(".messageBox")) {
+        document.querySelector(".messageBox").remove();
+      } else {
+        let textArea = document.querySelector(".textBox");
+        let textBox = document.createElement("textarea");
+        textBox.placeholder = "Your Message Here...";
+        textBox.classList.add("messageBox");
+        textBox.name = "message";
+        textBox.type = "text";
+        textArea.appendChild(textBox);
+      }
+    } else {
       this.lastElementChild.innerHTML = "Follow";
     }
   };
